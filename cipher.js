aaf42787b4dab9d70fa6768ec59e94bc20c62511
Your Name <you@example.com>
2021-06-18 12:47:41 -0300

    Updates

---
+++ cipher.js
@@ -0,0 +1,39 @@
+const cipher = {
+  encode: function (offset, word) {
+    if (typeof offset !== "number" || typeof word !== "string") {
+      throw new TypeError
+    }
+
+    let resultEncrypt = ""
+    for (let i = 0; i < word.length; i++) {
+      let encrypt = word.charCodeAt(i)
+      if (encrypt >= 65 && encrypt <= 90) {
+        encrypt = ((encrypt - 65 + offset) % 26) + 65
+      } else if (encrypt >= 97 && encrypt <= 122) {
+        encrypt = ((encrypt - 97 + offset) % 26) + 97
+      }
+      resultEncrypt += String.fromCharCode(encrypt)
+    }
+    return resultEncrypt
+  },
+
+  decode: function (offset, word) {
+    if (typeof offset !== "number" || typeof word !== "string") {
+      throw new TypeError
+    }
+
+    let resultDecrypt = ""
+    for (let i = 0; i < word.length; i++) {
+      let decrypt = word.charCodeAt(i)
+      if (decrypt >= 65 && decrypt <= 90) {
+        decrypt = ((decrypt - 90 - offset) % 26) + 90
+      } else if (decrypt >= 97 && decrypt <= 122) {
+        decrypt = ((decrypt - 122 - offset) % 26) + 122
+      }
+      resultDecrypt += String.fromCharCode(decrypt)
+    }
+    return resultDecrypt
+  }
+}
+
+export default cipher;
---
+++ index.js
@@ -0,0 +1,19 @@
+import cipher from './cipher.js';
+
+const offset = document.getElementById('offset')
+const encryptMsg = document.getElementById('encryptmsg')
+const encryptBtn = document.getElementById('btnencrypt')
+const decryptMsg = document.getElementById('decryptmsg')
+const decryptBtn = document.getElementById('btndecrypt')
+
+encryptBtn.addEventListener('click', () => {
+  const offsetValue = parseInt(offset.value)
+  const word = encryptMsg.value
+  document.getElementById('decryptmsg').value = cipher.encode(offsetValue, word)
+})
+
+decryptBtn.addEventListener('click', () => {
+  const offsetValue = parseInt(offset.value)
+  const word = decryptMsg.value
+  document.getElementById('encryptmsg').value = cipher.decode(offsetValue, word)
+})
