62655cf8d7f84b5923ca8c1774b18d3bb99cf6a4
Your Name <you@example.com>
2021-06-20 16:35:21 -0300

    Correção TypeError

--- src/index.js
+++ src/index.js
@@ -10,10 +10,12 @@ encryptBtn.addEventListener('click', () => {
   const offsetValue = parseInt(offset.value)
   const word = encryptMsg.value
   document.getElementById('decryptmsg').value = cipher.encode(offsetValue, word)
+  document.getElementById('encryptmsg').value = ""
 })
 
 decryptBtn.addEventListener('click', () => {
   const offsetValue = parseInt(offset.value)
   const word = decryptMsg.value
   document.getElementById('encryptmsg').value = cipher.decode(offsetValue, word)
+  document.getElementById('decryptmsg').value = ""
 })
